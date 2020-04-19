2bdba9c208971dc5a51abca21411a19ada56fa51
Happyguy3018 <43387693+Happyguy3018@users.noreply.github.com>
2020-04-19 23:10:35 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,147 +1,108 @@
+
 CREATE TABLE dim_publisher_sf
 (
-publisher_id integer,
 publisher char(100)
 );
 
 ALTER TABLE dim_publisher_sf
-ADD CONSTRAINT publisher_pk PRIMARY KEY (publisher_id);
+ADD CONSTRAINT publisher_pk PRIMARY KEY (publisher);
 
 -- 
 
 CREATE TABLE dim_genre_sf
 (
-genre_id integer,
 genre char(100)
 );
 
 ALTER TABLE dim_genre_sf
-ADD CONSTRAINT genre_pk PRIMARY KEY (genre_id);
+ADD CONSTRAINT genre_pk PRIMARY KEY (genre);
 
 -- 
 CREATE TABLE dim_developer_sf
 (
-developer_id integer,
 developer char(100)
 );
 
 ALTER TABLE dim_developer_sf
-ADD CONSTRAINT developer_pk PRIMARY KEY (developer_id);
+ADD CONSTRAINT developer_pk PRIMARY KEY (developer);
 
 --
 
 
 CREATE TABLE dim_game_sf
 (
-game_id integer,
-game_name char(100),
-genre_id integer,
-publisher_id integer,
-developer_id integer
+
+game char(100),
+genre char(100),
+publisher char(100),
+developer char(100)
 );
 
 ALTER TABLE dim_game_sf
-ADD CONSTRAINT game_pk PRIMARY KEY (game_id);
+ADD CONSTRAINT dim_game_sf_pk PRIMARY KEY (game);
   
   
 ALTER TABLE dim_game_sf
 ADD CONSTRAINT dim_game_sf_genre_fk
-  FOREIGN KEY (genre_id)
-  REFERENCES dim_genre_sf (genre_id);
+  FOREIGN KEY (genre)
+  REFERENCES dim_genre_sf (genre);
   
   
 ALTER TABLE dim_game_sf
 ADD CONSTRAINT dim_game_sf_developer_fk
-  FOREIGN KEY (developer_id)
-  REFERENCES dim_developer_sf (developer_id);
+  FOREIGN KEY (developer)
+  REFERENCES dim_developer_sf (developer);
   
   
 ALTER TABLE dim_game_sf
 ADD CONSTRAINT dim_game_sf_publisher_fk
-  FOREIGN KEY (publisher_id)
-  REFERENCES dim_publisher_sf (publisher_id);
+  FOREIGN KEY (publisher)
+  REFERENCES dim_publisher_sf (publisher);
 -- 
 
 
 CREATE TABLE dim_year_sf
 (
-year_id integer,
-year integer
+release_year integer
 );
 
 ALTER TABLE dim_year_sf
-ADD CONSTRAINT year_pk PRIMARY KEY (year_id);
---
-
-
-CREATE TABLE dim_month_sf
-(
-month_id integer,
-month char(9),
-year_id integer
-);
-
-ALTER TABLE dim_month_sf
-ADD CONSTRAINT month_pk PRIMARY KEY (month_id);
-
-ALTER TABLE dim_month_sf
-ADD CONSTRAINT dim_month_sf_year_fk
-  FOREIGN KEY (year_id)
-  REFERENCES dim_year_sf (year_id);
---
-
-
-CREATE TABLE dim_release_sf
-(
-release_id integer,
-day integer,
-month_id integer
-);
-
-ALTER TABLE dim_release_sf
-ADD CONSTRAINT release_pk PRIMARY KEY (release_id);
-
-ALTER TABLE dim_release_sf
-ADD CONSTRAINT dim_release_sf_month_fk
-  FOREIGN KEY (month_id)
-  REFERENCES dim_month_sf (month_id);
+ADD CONSTRAINT year_pk PRIMARY KEY (release_year);
 --
 
 
 CREATE TABLE dim_all_review_sf
 (
-all_review_id integer,
-all_review char(40)
+all_review char(100)
 );
 
 ALTER TABLE dim_all_review_sf
-ADD CONSTRAINT all_review_pk PRIMARY KEY (all_review_id);
+ADD CONSTRAINT all_review_pk PRIMARY KEY (all_review);
 --
 
 
 CREATE TABLE fact_game_sale
 (
 original_price float,
-game_id integer,
-release_id integer,
-all_review_id integer
+game char(100),
+release_year integer,
+all_review char(100)
 );
 
 ALTER TABLE fact_game_sale
-ADD CONSTRAINT game_id_pk PRIMARY KEY (game_id);
+ADD CONSTRAINT fact_game_sale_pk PRIMARY KEY (game);
 
 ALTER TABLE fact_game_sale
 ADD CONSTRAINT fact_game_sale_all_review_fk
-  FOREIGN KEY (all_review_id)
-  REFERENCES dim_all_review_sf (all_review_id);
+  FOREIGN KEY (all_review)
+  REFERENCES dim_all_review_sf (all_review);
   
 ALTER TABLE fact_game_sale
 ADD CONSTRAINT fact_game_sale_release_fk
-  FOREIGN KEY (release_id)
-  REFERENCES dim_release_sf (release_id);
+  FOREIGN KEY (release)
+  REFERENCES dim_release_sf (release);
 
 ALTER TABLE fact_game_sale
 ADD CONSTRAINT fact_game_sale_game_fk
-  FOREIGN KEY (game_id)
-  REFERENCES dim_game_sf (game_id);
-
+  FOREIGN KEY (game)
+  REFERENCES dim_game_sf (game);

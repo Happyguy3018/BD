4e193f6321f5c9361688f54fc2c977b0dba3c972
Happyguy3018 <43387693+Happyguy3018@users.noreply.github.com>
2020-04-20 07:28:11 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,21 +1,375 @@
-INSERT INTO dim_developer_sf(developer)
+INSERT INTO Developer (developer)
 VALUES('id Software');
+INSERT INTO Developer (developer)
+VALUES('PUBG Corporation');
+INSERT INTO Developer (developer)
+VALUES('Harebrained Schemes');
+INSERT INTO Developer (developer)
+VALUES('Bohemia Interactive');
+INSERT INTO Developer (developer)
+VALUES('CCP');
+INSERT INTO Developer (developer)
+VALUES('Rockstar North');
+INSERT INTO Developer (developer)
+VALUES('CAPCOM Co.');
+INSERT INTO Developer (developer)
+VALUES('No Brakes Games');
+INSERT INTO Developer (developer)
+VALUES('Numantian Games');
+INSERT INTO Developer (developer)
+VALUES('Eko Software');
+INSERT INTO Developer (developer)
+VALUES('IronOak Games');
+INSERT INTO Developer (developer)
+VALUES('Spike Chunsoft Co.');
+INSERT INTO Developer (developer)
+VALUES('Bluehole');
+INSERT INTO Developer (developer)
+VALUES('Raven Software');
+INSERT INTO Developer (developer)
+VALUES('Radiant Entertainment');
+INSERT INTO Developer (developer)
+VALUES('Paradox Development Studio');
+INSERT INTO Developer (developer)
+VALUES('Doborog Games');
+INSERT INTO Developer (developer)
+VALUES('BANDAI NAMCO Studios Inc.');
+INSERT INTO Developer (developer)
+VALUES('Pixonic');
+INSERT INTO Developer (developer)
+VALUES('343 Industries');
+INSERT INTO Developer (developer)
+VALUES('Treyarch');
+INSERT INTO Developer (developer)
+VALUES('Sumo Digital');
+INSERT INTO Developer (developer)
+VALUES('THQ Nordic');
+INSERT INTO Developer (developer)
+VALUES('Valve');
+INSERT INTO Developer (developer)
+VALUES('RuneStorm');
+INSERT INTO Developer (developer)
+VALUES('Beamdog');
 
-INSERT INTO dim_publisher_sf(publisher)
-VALUES('Bethesda Softworks');
-
-INSERT INTO dim_genre_sf(genre)
-VALUES('Action');
-
-INSERT INTO dim_game_sf(game, genre, publisher, developer)
-VALUES('DOOM', 'Action', 'Bethesda Softworks', 'id Software');
 
 
-INSERT INTO dim_all_review_sf(all_review)
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('id Software', 'Action');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('PUBG Corporation', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('PUBG Corporation', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('PUBG Corporation', 'Massively Multiplayer');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Harebrained Schemes', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Harebrained Schemes', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Harebrained Schemes', 'Strategy');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bohemia Interactive', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bohemia Interactive', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bohemia Interactive', 'Massively Multiplayer');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CCP', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CCP', 'Free to Play');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CCP', 'Massively Multiplayer');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CCP', 'RPG');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CCP', 'Strategy');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Rockstar North', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Rockstar North', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CAPCOM Co.', 'Action');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('No Brakes Games', 'Indie');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('No Brakes Games', 'Adventure');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Numantian Games', ',Early Access');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Numantian Games', 'Strategy');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Eko Software', 'RPG');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Eko Software', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Eko Software', 'Action');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('IronOak Games', 'Strategy');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('IronOak Games', 'RPG');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('IronOak Games', 'Indie');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('IronOak Games', 'Adventure');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Spike Chunsoft Co.', 'Adventure');
+
+
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bluehole', 'Massively Multiplayer');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bluehole', 'Free to Play');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bluehole', 'RPG');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bluehole', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Bluehole', 'Action');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Raven Software', 'Action');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Radiant Entertainment', 'Strategy');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Radiant Entertainment', 'Simulation');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Radiant Entertainment', 'Indie');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Paradox Development Studio', 'Strategy');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Paradox Development Studio', 'Simulation');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Doborog Games', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Doborog Games', 'Indie');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Doborog Games', 'Early ');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('BANDAI NAMCO Studios Inc.', 'Action');
+
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Pixonic', 'Free to Play	');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Pixonic', 'Action');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('343 Industries', 'Strategy');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Treyarch', 'Action');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('CAPCOM Co.', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Sumo Digital', 'Racing');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('THQ Nordic', 'Adventure');
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('THQ Nordic', 'Action');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Valve', 'Adventure');
+
+INSERT INTO Developer_Genre (developer, genre)
+VALUES('Valve', 'Action');
+
+
+ 
+ 
+INSERT INTO Developer_Game (developer, game, publisher)
+VALUES('id Software', 'DOOM', 'Bethesda Softworks');
+INSERT INTO Developer_Game (developer, game)
+VALUES('PUBG Corporation', 'PLAYERUNKNOWNS BATTLEGROUNDS');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Harebrained Schemes', 'BATTLETECH');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Bohemia Interactive', 'DayZ');
+INSERT INTO Developer_Game (developer, game)
+VALUES('CCP', 'EVE Online	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Rockstar North', 'Grand Theft Auto V: Premium Online Edition');
+INSERT INTO Developer_Game (developer, game)
+VALUES('CAPCOM Co.', 'Devil May Cry 5	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('No Brakes Games', 'Human: Fall Flat	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Numantian Games', 'They Are Billions	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Eko Software', 'Warhammer: Chaosbane	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('IronOak Games', 'For The King	');
+
+
+INSERT INTO Developer_Game (developer, game)
+VALUES('Spike Chunsoft Co.', '	Danganronpa V3: Killing Harmony');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Bluehole', 'TERA');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Raven Software', 'Call of Duty®: Modern Warfare® Remastered');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Radiant Entertainment', 'Stonehearth');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Paradox Development Studio', 'Hearts of Iron IV: Mobilization Pack	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Doborog Games', 'Clone Drone in the Danger Zone	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('BANDAI NAMCO Studios Inc.', 'EATER ');
+
+
+INSERT INTO Developer_Game (developer, game)
+VALUES('Pixonic', 'War Robots	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('343 Industries', 'Halo Wars: Definitive Edition');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Treyarch', 'Call of Duty®: Black Ops');
+
+INSERT INTO Developer_Game (developer, game)
+VALUES('CAPCOM Co.', 'Phoenix Wright: Ace Attorney Trilogy');
+
+INSERT INTO Developer_Game (developer, game)
+VALUES('Sumo Digital', 'Team Sonic Racing™');
+INSERT INTO Developer_Game (developer, game)
+VALUES('THQ Nordic', '	Titan Quest Anniversary Edition');
+
+INSERT INTO Developer_Game (developer, game)
+VALUES('Rockstar Nort', 'Grand Theft Auto IV');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Valve', 'Portal Bundle	');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Valve', 'Portal');
+INSERT INTO Developer_Game (developer, game)
+VALUES('RuneStorm', '	Viscera Cleanup Detail');
+INSERT INTO Developer_Game (developer, game)
+VALUES('Beamdog', 'Neverwinter Nights: Enhanced Edition');
+ 
+
+
+INSERT INTO Game_title (game)
+VALUES('DOOM');
+INSERT INTO Game_title (game)
+VALUES('PLAYERUNKNOWNS BATTLEGROUNDS	');
+INSERT INTO Game_title (game)
+VALUES('DayZ');
+INSERT INTO Game_title (game)
+VALUES('BATTLETECH');
+INSERT INTO Game_title (game)
+VALUES('EVE Online');
+INSERT INTO Game_title (game)
+VALUES('Grand Theft Auto V: Premium Online Edition	');
+INSERT INTO Game_title (game)
+VALUES('Devil May Cry 5	');
+INSERT INTO Game_title (game)
+VALUES('Human: Fall Flat	');
+INSERT INTO Game_title (game)
+VALUES('They Are Billions	');
+INSERT INTO Game_title (game)
+VALUES('Warhammer: Chaosbane	');
+INSERT INTO Game_title (game)
+VALUES('For The King	');
+INSERT INTO Game_title (game)
+VALUES('Danganronpa V3: Killing Harmony	');
+INSERT INTO Game_title (game)
+VALUES('TERA');
+INSERT INTO Game_title (game)
+VALUES('Call of Duty®: Modern Warfare® Remastered	');
+INSERT INTO Game_title (game)
+VALUES('Stonehearth');
+INSERT INTO Game_title (game)
+VALUES('Hearts of Iron IV: Mobilization Pack	');
+INSERT INTO Game_title (game)
+VALUES('Clone Drone in the Danger Zone	');
+INSERT INTO Game_title (game)
+VALUES('GOD EATER 3	');
+INSERT INTO Game_title (game)
+VALUES('War Robots	');
+INSERT INTO Game_title (game)
+VALUES('Halo Wars: Definitive Edition	');
+INSERT INTO Game_title (game)
+VALUES('Call of Duty®: Black Ops	');
+INSERT INTO Game_title (game)
+VALUES('Phoenix Wright: Ace Attorney Trilogy');
+INSERT INTO Game_title (game)
+VALUES('Team Sonic Racing™	');
+INSERT INTO Game_title (game)
+VALUES('Titan Quest Anniversary Edition	');
+INSERT INTO Game_title (game)
+VALUES('Grand Theft Auto IV	');
+INSERT INTO Game_title (game)
+VALUES('Portal Bundle	');
+INSERT INTO Game_title (game)
+VALUES('Portal');
+INSERT INTO Game_title (game)
+VALUES('Viscera Cleanup Detail	');
+INSERT INTO Game_title (game)
+VALUES('Neverwinter Nights: Enhanced Edition');
+
+INSERT INTO Review (review)
 VALUES('Very Positive');
+INSERT INTO Review (review)
+VALUES('Mixed');
+INSERT INTO Review (review)
+VALUES('Mostly Positive');
+INSERT INTO Review (review)
+VALUES('NaN');
+INSERT INTO Review (review)
+VALUES('Overwhelmingly Positive');
 
-INSERT INTO dim_year_sf (release_year)
-VALUES (2016)
 
-INSERT INTO fact_game_sale(original_price, game, release_year, all_review)
-VALUES(19.99, 'DOOM', 2016,'Very Positive');
+INSERT INTO R_year (r_year)
+VALUES(2003);
+INSERT INTO R_year (r_year)
+VALUES(2016);
+INSERT INTO R_year (r_year)
+VALUES(2017);
+INSERT INTO R_year (r_year)
+VALUES(2018);
+INSERT INTO R_year (r_year)
+VALUES(2019);
+INSERT INTO R_year (r_year)
+VALUES(2015);
+INSERT INTO R_year (r_year)
+VALUES(2010);
+INSERT INTO R_year (r_year)
+VALUES(2008);
+INSERT INTO R_year (r_year)
+VALUES(2007);
+
+INSERT INTO Game_title (game)
+VALUES ('Eve Online');
+
+INSERT INTO Game (price, game, r_year, review)
+VALUES(19.99,'DOOM','2016','Very Positive');
+INSERT INTO Game (price, game, r_year, review)
+VALUES(29.99,'PLAYERUNKNOWNS BATTLEGROUNDS','2017','Mixed');
+INSERT INTO Game (price, game, r_year, review)
+VALUES(39.99,'BATTLETECH','2018','Mostly Positive');
+INSERT INTO Game (price, game, r_year, review)
+VALUES(44.99,'DayZ','2018','Mixed');
+INSERT INTO Game (price, game, r_year, review)
+VALUES(0,'EVE Online','2003','Mostly Positive,');
+
+  

ca0523bd46d99c3bb20313966b122c2cf774d717
Happyguy3018 <43387693+Happyguy3018@users.noreply.github.com>
2020-04-20 07:48:49 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,31 +1,31 @@
 --Вивести кількість ігор випущених за кожен рік.
-select dim_year_sf.release_year, count(game)
-from fact_game_sale
-INNER JOIN dim_year_sf
-ON fact_game_sale.release_year = dim_year_sf.release_year
-GROUP BY dim_year_sf.release_year;
-
+select count(Game_title.game), R_year.r_year
+from Game
+INNER JOIN R_year
+ON Game.r_year = R_year.r_year
+INNER JOIN Game_title
+ON Game.game = Game_title.game
+GROUP BY  R_year.r_year;
 
 -- Вивести відсоткову залежність кількості ігор одного жанру відносно інших.
-select count(dim_game_sf.game), dim_genre_sf.genre
-from dim_game_sf
-INNER JOIN dim_genre_sf
-ON dim_genre_sf.genre = dim_game_sf.genre
-GROUP BY dim_genre_sf.genre
+select count(Game_title.game), Developer_Genre.genre  from
+Game INNER JOIN (Game_title INNER JOIN (Developer_Game INNER JOIN (Developer INNER JOIN Developer_Genre ON Developer.developer = Developer_genre.developer) ON Developer_Game.developer = Developer.developer) ON Game_title.game = developer_game.game) on Game.game = Game_title.game
+INNER JOIN R_year on Game.r_year = R_year.r_year
+GROUP BY Developer_Genre.genre;
 
-
---Динамыка кылькосты ігор жанру 'Strategy' по роках
-select  
- count(dim_game_sf.game), dim_year_sf.release_year
-
-from fact_game_sale INNER JOIN 
-    (
-        dim_game_sf
-        INNER JOIN dim_genre_sf
-        ON dim_genre_sf.genre = dim_game_sf.genre
-    )
-    ON fact_game_sale.game = dim_game_sf.game
-    INNER JOIN dim_year_sf
-    ON fact_game_sale.release_year = dim_year_sf.release_year
-where dim_genre_sf.genre = 'Strategy'
-GROUP BY dim_year_sf.release_year;
+--Динамыка кылькосты ігор жанру 'Action' по роках
+select R_year.r_year, count(Game_title.game)
+FROM Game INNER JOIN (
+                Game_title INNER JOIN 
+                    (
+                        Developer_Game INNER JOIN 
+                        (
+                            Developer INNER JOIN Developer_Genre ON Developer.developer = Developer_genre.developer
+                        ) 
+                        ON Developer_Game.developer = Developer.developer
+                    ) 
+                    ON Game_title.game = developer_game.game
+                ) on Game.game = Game_title.game
+INNER JOIN R_year on Game.r_year = R_year.r_year
+where TRIM(Developer_Genre.genre) = 'Action'
+Group BY R_year.r_year;

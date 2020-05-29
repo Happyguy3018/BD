1acb95ab146debc16d6867d31a4b5e2231389f79
Happyguy3018 <43387693+Happyguy3018@users.noreply.github.com>
2020-05-29 10:30:59 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,34 +1,13 @@
-CREATE TABLE GAME
-(
-price float,
-game char(64),
-r_year integer,
-review char(64)
-);
-CREATE TABLE Game_title
-(
-game char(64)
-);
-
 CREATE TABLE Developer
 (
 developer char(64)
 );
-
-ALTER TABLE Developer
-ADD CONSTRAINT developer_pk PRIMARY KEY (developer);
-
 
 CREATE TABLE Developer_Genre
 (
 developer char(64),
 genre char(64)
 );
-
-ALTER TABLE Developer_Genre
-ADD CONSTRAINT developer_genre_pk PRIMARY KEY (developer, genre);
-
-
 
 CREATE TABLE Developer_Game
 (
@@ -37,18 +16,11 @@ game char(64),
 publisher char(64)
 );
 
-ALTER TABLE Developer_Game
-ADD CONSTRAINT developer_game_pk PRIMARY KEY (developer, game);
-
-
  
 CREATE TABLE Review
 (
 review char(64)
 );
-
-ALTER TABLE Review
-ADD CONSTRAINT review_pk PRIMARY KEY (review);
 
 
 CREATE TABLE R_year
@@ -56,18 +28,12 @@ CREATE TABLE R_year
 r_year integer
 );
 
-ALTER TABLE R_year
-ADD CONSTRAINT ryear_pk PRIMARY KEY (r_year);
 
 
 CREATE TABLE Game_title
 (
 game char(64)
 );
-
-ALTER TABLE Game_title
-ADD CONSTRAINT game_title_pk PRIMARY KEY (game);
-
 
 
 CREATE TABLE GAME
@@ -77,10 +43,28 @@ game char(64),
 r_year integer,
 review char(64)
 );
-ALTER TABLE Game_title
-ADD CONSTRAINT game_title_pk PRIMARY KEY (game);
+
 ALTER TABLE Game
 ADD CONSTRAINT game_pk PRIMARY KEY (game);
+
+ALTER TABLE Developer_Game
+ADD CONSTRAINT developer_game_pk PRIMARY KEY (developer, game);
+
+ALTER TABLE Developer
+ADD CONSTRAINT developer_pk PRIMARY KEY (developer);
+
+ALTER TABLE Developer_Genre
+ADD CONSTRAINT developer_genre_pk PRIMARY KEY (developer, genre);
+
+ALTER TABLE Review
+ADD CONSTRAINT review_pk PRIMARY KEY (review);
+
+ALTER TABLE Game_title
+ADD CONSTRAINT game_title_pk PRIMARY KEY (game);
+
+ 
+ALTER TABLE R_year
+ADD CONSTRAINT ryear_pk PRIMARY KEY (r_year);
 
 ALTER TABLE Game
 ADD CONSTRAINT Game_Game_title_fk
@@ -97,14 +81,15 @@ ADD CONSTRAINT Game_Ryear_fk
  FOREIGN KEY (r_year)
  REFERENCES R_year (r_year);
  
- 
+
 
 ALTER TABLE Developer_Genre
 ADD CONSTRAINT Developer_Genre_Developer_fk
  FOREIGN KEY (developer)
  REFERENCES Developer (developer);
-
-  ALTER TABLE Developer_Game
+ 
+ 
+ALTER TABLE Developer_Game
 ADD CONSTRAINT Developer_Game_Developer_fk
  FOREIGN KEY (developer)
  REFERENCES Developer (developer);
@@ -113,4 +98,5 @@ ALTER TABLE Developer_Game
 ADD CONSTRAINT Developer_Game_Game_fk
  FOREIGN KEY (game)
  REFERENCES Game_title (game);
+
 
